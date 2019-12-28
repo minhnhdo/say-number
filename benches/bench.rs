faed181c41642bce878480ba7d97f561eb497404
Do Nhat Minh <minhnhdo@gmail.com>
2019-12-28 15:44:36 -0500

    added benchmarks

---
+++ benches/bench.rs
@@ -0,0 +1,21 @@
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use say_number::say;
+
+fn say_0(c: &mut Criterion) {
+    c.bench_function("say 0", |b| b.iter(|| say(black_box(0))));
+}
+
+fn say_42(c: &mut Criterion) {
+    c.bench_function("say 42", |b| b.iter(|| say(black_box(42))));
+}
+
+fn say_514(c: &mut Criterion) {
+    c.bench_function("say 514", |b| b.iter(|| say(black_box(514))));
+}
+
+fn say_max_u64(c: &mut Criterion) {
+    c.bench_function("say max u64", |b| b.iter(|| say(black_box(std::u64::MAX))));
+}
+
+criterion_group!(benches, say_max_u64, say_514, say_42, say_0);
+criterion_main!(benches);
