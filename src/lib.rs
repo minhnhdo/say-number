7b09e42139b8eccc8443287bfc67eeee381fe44f
Do Nhat Minh <minhnhdo@gmail.com>
2019-12-29 23:07:21 -0500

    added docs

--- src/lib.rs
+++ src/lib.rs
@@ -14,6 +14,9 @@
 //! ```rust
 //! println!("The number is {}.", say_number::say(42));
 //! assert_eq!(say_number::say(514), "five hundred and fourteen".to_string());
+//!  assert_eq!(say_number::english::say(514), "five hundred and fourteen".to_string());
+//! assert_eq!(say_number::french::say(514), "cinq-cent-quatorze".to_string());
+//! assert_eq!(say_number::vietnamese::say(514), "năm trăm mười bốn".to_string());
 //! ```
 
 pub mod english;
