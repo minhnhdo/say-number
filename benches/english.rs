e2dac3c638821ccd3a6ac69ffd654cfab3e2a691
Do Nhat Minh <minhnhdo@gmail.com>
2020-01-02 21:29:48 -0500

    more descriptive benchmarks

--- benches/english.rs
+++ benches/english.rs
@@ -2,19 +2,21 @@ use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use say_number::english::say;
 
 fn say_0(c: &mut Criterion) {
-    c.bench_function("say 0", |b| b.iter(|| say(black_box(0))));
+    c.bench_function("say 0 in English", |b| b.iter(|| say(black_box(0))));
 }
 
 fn say_42(c: &mut Criterion) {
-    c.bench_function("say 42", |b| b.iter(|| say(black_box(42))));
+    c.bench_function("say 42 in English", |b| b.iter(|| say(black_box(42))));
 }
 
 fn say_514(c: &mut Criterion) {
-    c.bench_function("say 514", |b| b.iter(|| say(black_box(514))));
+    c.bench_function("say 514 in English", |b| b.iter(|| say(black_box(514))));
 }
 
 fn say_max_u64(c: &mut Criterion) {
-    c.bench_function("say max u64", |b| b.iter(|| say(black_box(std::u64::MAX))));
+    c.bench_function("say max u64 in English", |b| {
+        b.iter(|| say(black_box(std::u64::MAX)))
+    });
 }
 
 criterion_group!(english, say_max_u64, say_514, say_42, say_0);
